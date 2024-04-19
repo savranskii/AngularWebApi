c2d49c88bf6180a7a1456e42b7393a1ae36a855c
Taras <taras.savranskii@outlook.com>
2024-04-19 17:42:31 +0400

    Add project files.

---
+++ AngularWebApi.Server/Extensions/CorsConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace AngularWebApi.Server.Extensions;
+
+public static class CorsConfiguration
+{
+    public const string PolicyName = "_ui";
+
+    public static void AddCorsConfig(this IServiceCollection services, ConfigurationManager configuration)
+    {
+        services.AddCors(o => o.AddPolicy(PolicyName, c => c
+            .WithOrigins("https://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod()));
+    }
+}
